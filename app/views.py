2e5dd9042f1573b81b803068007506a0bd8f3785
Edwin <edbeej@gmail.com>
2017-03-20 00:07:28 -0500

    search bar added

--- app/views.py
+++ app/views.py
@@ -4,13 +4,11 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", restaurants = [{"name" : "Gato", "img": "gato.jpg"}, {"name" : "Stack Burgerz", "img": "stack.jpg"}, {"name" : "Little Italy", "img": "italy.jpg"}])
+    return render_template("index.html")
 
-
-@app.route('/Restaurants')
+@app.route('/restaurants')
 def Restaurants():
-    # return render_template(blah)
-    return "Restaurants Table Page"
+    return render_template("grid.html", restaurants = [{"name" : "Gato", "img": "gato.jpg"}, {"name" : "Stack Burgerz", "img": "stack.jpg"}, {"name" : "Little Italy", "img": "italy.jpg"}])
 
 # the route will be dynamic in the future.
 @app.route('/restaurant1')
