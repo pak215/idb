26d55b9934e0b24af6f4d894b113f5bc9b255236
pabloeckardt <pablo0168@hotmail.com>
2017-03-22 01:27:17 -0500

    made restaurants spoof db global, and made details for restaurantss in /Restaurants available

--- app/views.py
+++ app/views.py
@@ -1,5 +1,39 @@
 from flask import render_template, Blueprint, render_template
-
+spoof_db =[
+    {"name" : "Little Italy",\
+     "img": "italy.jpeg", \
+    "last review" : "Excellent Garlic Bread. -Tom", \
+    "location" : "1215 S Congress", \
+    "zip" : "78701", \
+    "price" : "$$", \
+    "hours": "11:00am to 11:00pm", \
+    "food type": "Italian Food", \
+    "rating": "3.4",\
+    "img": "italy.jpg",\
+    "id" : "1"
+    },
+    {"name" : "Gato",\
+     "img": "gato.jpg", \
+    "last review" : "They actually don't serve cat 1/10. -Tom", \
+    "location" : "5568 N Lamar Blvd", \
+    "zip" : "78702", \
+    "price" : "$$$", \
+    "hours": "11:00am to 9:00pm", \
+    "food type": "Mediterranean Food", \
+    "rating": "4",\
+    "id" : "2"
+    }, 
+    {"name" : "Stack Burgers",\
+    "img": "stack.jpg", \
+    "last review" : "I tried their waffles, not impressed. -Jen", \
+    "location" : "1231 Wells Branch Pwy", \
+    "zip" : "78703", \
+    "price" : "$", \
+    "hours": "11:00am to 11:00am", \
+    "food type": "American Food", \
+    "rating": "5",\
+    "id" : "3"
+    }]
 views = Blueprint('views', __name__)
 
 @views.route('/')
@@ -10,14 +44,8 @@ def index():
 
 @views.route('/Restaurants')
 def Restaurants():
-    return render_template(
-        "restaurants.html",\
-        model_elements = [
-            {"name" : "Gato", "img": "gato.jpg", "id":2},\
-            {"name" : "Stack Burgerz", "img": "stack.jpg", "id":3},\
-            {"name" : "Little Italy", "img": "italy.jpg", "id":1}
-        ])
-
+    global spoof_db
+    return render_template("restaurants.html", model_elements = spoof_db)
 
 @views.route('/Locations')
 def Locations():
@@ -29,7 +57,6 @@ def Locations():
             {"name" : "78703", "img": "temp_image.png"}
         ])
 
-
 @views.route('/Food_Types')
 def Food_types():
     return render_template(
@@ -39,7 +66,6 @@ def Food_types():
             {"name" : "American", "img": "temp_image.png"},\
             {"name" : "Spanish", "img": "temp_image.png"}
         ])
-
 
 @views.route('/Reviews')
 def Reviews():
@@ -51,41 +77,10 @@ def Reviews():
             {"name" : "Little Italy - Tom", "img": "temp_image.png"}
         ])
 
-
-
 # Model Elements Views
 @views.route('/Restaurants/<pk>')
 def restaurant(pk):
-    # should only work with restaurant primary key = 1
-    spoof_db =[{}, {"name" : "Little Italy", "img": "italy.jpeg", \
-        "last review" : "Excellent Garlic Bread. -Tom", \
-        "location" : "1215 S Congress", \
-        "zip" : "78701", \
-        "price" : "$$", \
-        "hours": "11:00am to 11:00pm", \
-        "food type": "Italian Food", \
-        "rating": "4.5",\
-        "img": "italy.jpg"
-        },
-        {"name" : "Little Italy", "img": "italy.jpeg", \
-        "last review" : "Excellent Garlic Bread. -Tom", \
-        "location" : "1215 S Congress", \
-        "zip" : "78701", \
-        "price" : "$$", \
-        "hours": "11:00am to 11:00pm", \
-        "food type": "Italian Food", \
-        "rating": "4.5",\
-        "img": "italy.jpg"
-        }, 
-            {"name" : "Little Italy", "img": "italy.jpeg", \
-	    "last review" : "Excellent Garlic Bread. -Tom", \
-	    "location" : "1215 S Congress", \
-	    "zip" : "78701", \
-	    "price" : "$$", \
-	    "hours": "11:00am to 11:00pm", \
-	    "food type": "Italian Food", \
-	    "rating": "4.5",\
-	    "img": "italy.jpg"
-	    }]
-
-    return render_template("restaurant_instance.html",instance=spoof_db[int(pk)])
+    global spoof_db
+    for d in spoof_db:
+	if d["id"] == pk:
+	    return render_template("restaurant_instance.html",instance=d)
